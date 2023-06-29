fb1589f9d78aca3e2e6a0c69a30c8b92e22bafed
unknown <marcos.eugenioln@gmail.com>
2023-06-29 11:50:47 -0300

    add login page

--- signin.php
+++ signin.php
@@ -1,131 +1,141 @@
 <?php include ( "inc/connect.inc.php" ); ?>
+
 <?php
+
 ob_start();
 session_start();
-if (!isset($_SESSION['user_login'])) {
-}
-else {
+
+if (isset($_SESSION['user_id'])) 
+{
 	header("location: index.php");
 }
 
-$u_fname = "";
-$u_lname = "";
-$u_email = "";
-$u_mobile = "";
-$u_address = "";
-$u_pass = "";
+if (isset($_POST['signup'])) 
+{
+	//declere veriable
+	$user_first_name = trim($_POST['first_name']);
+	$user_last_name = trim($_POST['last_name']);
+	$user_email = $_POST['email'];
+	$user_mobile = $_POST['mobile'];
+	$user_address = $_POST['signupaddress'];
+	$user_pass = $_POST['password'];
 
-if (isset($_POST['signup'])) {
-//declere veriable
-$u_fname = $_POST['first_name'];
-$u_lname = $_POST['last_name'];
-$u_email = $_POST['email'];
-$u_mobile = $_POST['mobile'];
-$u_address = $_POST['signupaddress'];
-$u_pass = $_POST['password'];
-//triming name
-$_POST['first_name'] = trim($_POST['first_name']);
-$_POST['last_name'] = trim($_POST['last_name']);
-	try {
-		if(empty($_POST['first_name'])) {
+
+	try 
+	{
+		if(empty($_POST['first_name'])) 
+		{
 			throw new Exception('Fullname can not be empty');
-			
 		}
-		if (is_numeric($_POST['first_name'][0])) {
+		if (is_numeric($_POST['first_name'][0])) 
+		{
 			throw new Exception('Please write your correct name!');
-
 		}
-		if(empty($_POST['last_name'])) {
+		if(empty($_POST['last_name'])) 
+		{
 			throw new Exception('Lastname can not be empty');
-			
 		}
-		if (is_numeric($_POST['last_name'][0])) {
+		if (is_numeric($_POST['last_name'][0])) 
+		{
 			throw new Exception('lastname first character must be a letter!');
-
 		}
-		if(empty($_POST['email'])) {
+		if(empty($_POST['email'])) 
+		{
 			throw new Exception('Email can not be empty');
-			
 		}
-		if(empty($_POST['mobile'])) {
+		if(empty($_POST['mobile'])) 
+		{
 			throw new Exception('Mobile can not be empty');
-			
 		}
-		if(empty($_POST['password'])) {
+		if(empty($_POST['password'])) 
+		{
 			throw new Exception('Password can not be empty');
-			
 		}
-		if(empty($_POST['signupaddress'])) {
+		if(empty($_POST['signupaddress'])) 
+		{
 			throw new Exception('Address can not be empty');
-			
 		}
 
 		
 		// Check if email already exists
-		
 		$check = 0;
 		$e_check = mysqli_query($con, "SELECT email FROM `user` WHERE email='$u_email'");
 		$email_check = mysqli_num_rows($e_check);
-		if (strlen($_POST['first_name']) >2 && strlen($_POST['first_name']) <20 ) {
-			if (strlen($_POST['last_name']) >2 && strlen($_POST['last_name']) <20 ) {
-			if ($check == 0 ) {
-				if ($email_check == 0) {
-					if (strlen($_POST['password']) >1 ) {
-						$d = date("Y-m-d"); //Year - Month - Day
-						$_POST['first_name'] = ucwords($_POST['first_name']);
-						$_POST['last_name'] = ucwords($_POST['last_name']);
-						$_POST['last_name'] = ucwords($_POST['last_name']);
-						$_POST['email'] = mb_convert_case($u_email, MB_CASE_LOWER, "UTF-8");
-						$_POST['password'] = md5($_POST['password']);
-						$confirmCode   = substr( rand() * 900000 + 100000, 0, 6 );
-						// send email
-						$msg = "
-						...
-						
-						Your activation code: ".$confirmCode."
-						Signup email: ".$_POST['email']."
-						
-						";
-						//if (@mail($_POST['email'],"eBuyBD Activation Code",$msg, "From:eBuyBD <no-reply@ebuybd.xyz>")) {
+		
+		if (strlen($_POST['first_name']) >2 && strlen($_POST['first_name']) <20 ) 
+		{
+			if (strlen($_POST['last_name']) >2 && strlen($_POST['last_name']) <20 ) 
+			{
+				if ($check == 0 ) 
+				{
+					if ($email_check == 0) 
+					{
+						if (strlen($_POST['password']) >1 ) 
+						{
+							$d = date("Y-m-d"); //Year - Month - Day
+							$_POST['first_name'] = ucwords($_POST['first_name']);
+							$_POST['last_name'] = ucwords($_POST['last_name']);
+							$_POST['last_name'] = ucwords($_POST['last_name']);
+							$_POST['email'] = mb_convert_case($u_email, MB_CASE_LOWER, "UTF-8");
+							$_POST['password'] = md5($_POST['password']);
+
+							$confirmCode   = substr( rand() * 900000 + 100000, 0, 6 );
+							// send email
+							$msg = "
+							...
 							
-						$result = mysqli_query($con, "INSERT INTO user (firstName,lastName,email,mobile,address,password,confirmCode) VALUES ('$_POST[first_name]','$_POST[last_name]','$_POST[email]','$_POST[mobile]','$_POST[signupaddress]','$_POST[password]','$confirmCode')");
-						
-						//success message
-						$success_message = '
-						<div class="signupform_content"><h2><font face="bookman">Registration successfull!</font></h2>
-						<div class="signupform_text" style="font-size: 18px; text-align: center;">
-						<font face="bookman">
-							Email: '.$u_email.'<br>
-							Activation code sent to your email. <br>
-							Your activation code: '.$confirmCode.'
-						</font></div></div>';
-						//}else {
-							//throw new Exception('Email is not valid!');
-						//}
-						
-						
-					}else {
-						throw new Exception('Make strong password!');
+							Your activation code: ".$confirmCode."
+							Signup email: ".$_POST['email']."
+							
+							";
+							//if (@mail($_POST['email'],"eBuyBD Activation Code",$msg, "From:eBuyBD <no-reply@ebuybd.xyz>")) {
+								
+							$result = mysqli_query($con, "INSERT INTO user (firstName,lastName,email,mobile,address,password,confirmCode) VALUES ('$_POST[first_name]','$_POST[last_name]','$_POST[email]','$_POST[mobile]','$_POST[signupaddress]','$_POST[password]','$confirmCode')");
+							
+							//success message
+							$success_message = 
+							'
+								<div class="signupform_content"><h2><font face="bookman">Registration successfull!</font></h2>
+								<div class="signupform_text" style="font-size: 18px; text-align: center;">
+								<font face="bookman">
+									Email: '.$u_email.'<br>
+									Activation code sent to your email. <br>
+									Your activation code: '.$confirmCode.'
+								</font></div></div>
+							';
+						}
+						else 
+						{
+							throw new Exception('Make strong password!');
+						}
 					}
-				}else {
-					throw new Exception('Email already taken!');
+					else 
+					{
+						throw new Exception('Email already taken!');
+					}
 				}
-			}else {
-				throw new Exception('Username already taken!');
+				else 
+				{
+					throw new Exception('Username already taken!');
+				}
 			}
-			}else {
-			throw new Exception('Lastname must be 2-20 characters!');
+			else 
+			{
+				throw new Exception('Lastname must be 2-20 characters!');
+			}
 		}
-		}else {
+		
+		else 
+		{
 			throw new Exception('Firstname must be 2-20 characters!');
 		}
 
 	}
-	catch(Exception $e) {
+	catch(Exception $e) 
+	{
 		$error_message = $e->getMessage();
 	}
 }
-
 
 ?>
 
